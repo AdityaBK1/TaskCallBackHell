3519ff82b0b756cc82e5c384823613322742d461
AdityaBK1 <131005390+AdityaBK1@users.noreply.github.com>
2023-05-18 17:42:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+setTimeout(()=>{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            setTimeout(()=>{
+                setTimeout(()=>{
+                    setTimeout(() => {
+                        setTimeout(() => {
+                            setTimeout(() => {
+                                setTimeout(() => {
+                                    setTimeout(() => {
+                                        setTimeout(() => {
+                                            let h1 = document.createElement('h1')
+                        h1.innerHTML = "Happy Independence Day"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                                        let h1 = document.createElement('h1')
+                        h1.innerHTML = "1"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                    let h1 = document.createElement('h1')
+                        h1.innerHTML = "2"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                    let h1 = document.createElement('h1')
+                        h1.innerHTML = "3"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                    let h1 = document.createElement('h1')
+                        h1.innerHTML = "4"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                        let h1 = document.createElement('h1')
+                        h1.innerHTML = "5"
+                        document.body.appendChild(h1)
+                    }, 1000);
+                    let h1 = document.createElement('h1')
+                    h1.innerHTML = "6"
+                    document.body.appendChild(h1)
+                },1000)
+                let h1 = document.createElement('h1')
+                h1.innerHTML = "7"
+                document.body.appendChild(h1)
+            },1000)
+
+            let h1 = document.createElement('h1')
+            h1.innerHTML = "8"
+            document.body.appendChild(h1)
+        },1000)
+
+        let h1 = document.createElement('h1')
+        h1.innerHTML = "9"
+        document.body.appendChild(h1)
+    },1000)
+
+    let h1 = document.createElement('h1')
+    h1.innerHTML = "10"
+    document.body.appendChild(h1)
+},1000)
